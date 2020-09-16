50adff0b1c2106abafe1ac4e54b9efeb61d7109f
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-09-16 15:57:43 +0100

    set folder

--- BUG/py-symengine/work/symengine-0.6.1/symengine/lib/CMakeFiles/cython.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cython"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cython.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
