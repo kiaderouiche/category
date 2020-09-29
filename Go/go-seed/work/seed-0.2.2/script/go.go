11daf544a4587db64c82ae408936ffb0061e1e39
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-09-29 22:14:15 +0100

    +clean folder

--- Go/go-seed/work/seed-0.2.2/script/go.go
+++
@@ -1,8 +0,0 @@
-package script
-
-import qlova "github.com/qlova/script"
-
-//Go calls a Go function with the provided arguments.
-func (q Ctx) Go(function interface{}, args ...qlova.Type) Promise {
-	return q.rpc(function, "undefined", nil, args...)
-}
