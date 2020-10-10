7f8314e9b81a6831f63f34c4d977a6ffb261c4b3
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-10 16:55:20 +0100

    develop create options for pkgsrc components

--- krename/options.mk
+++ krename/options.mk
@@ -6,10 +6,35 @@ PKG_SUPPORTED_OPTIONS=		taglib podofo exiv2 freetype
 .include "../../mk/bsd.options.mk"
 
 ###
-### Lua support
+### freetype support
 ###
-.if !empty(PKG_OPTIONS:Mlua)
+.if !empty(PKG_OPTIONS:freetype)
+.include "../../graphics/freetype2/buildlink3.mk"
+CMAKE_ARGS+=	-DLUA_ENABLED:LUA_ENABLED=true
+.endif
+
+###
+### podofo support
+###
+.if !empty(PKG_OPTIONS:podofo)
 LUA_VERSIONS_INCOMPATIBLE=	52
 .  include "../../lang/lua/buildlink3.mk"
+CMAKE_ARGS+=	-DLUA_ENABLED:LUA_ENABLED=true
+.endif
+
+###
+### taglib support
+###
+.if !empty(PKG_OPTIONS:Mtaglib)
+BUILDLINK_API_DEPENDS.taglib+=	taglib>=1.0
+.include "../../audio/taglib/buildlink3.mk"
+CMAKE_ARGS+=	-DLUA_ENABLED:LUA_ENABLED=true
+.endif
+
+###
+### exiv2 support
+###
+.if !empty(PKG_OPTIONS:Mexiv2)
+.include "../../audio/taglib/buildlink3.mk"
 CMAKE_ARGS+=	-DLUA_ENABLED:LUA_ENABLED=true
 .endif
