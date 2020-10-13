8a8b8e356d5c6418657c706c04cce385c8369d01
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-13 11:02:51 +0100

    +comment line

---
+++ pkgoptions/options.mk
@@ -0,0 +1,10 @@
+# $NetBSD$
+# XXX
+# XXX This file was created automatically using pkgoptions-1.0.
+# XXX After this file has been verified as correct, the comment lines
+# XXX beginning with "XXX" should be removed.  Please do not commit
+# XXX unverified options.mk files.
+# XXX
+# XXX
+
+BUILDLINK_PKGSRCDIR.pkgoptions?=	../../category/pkgoptions
