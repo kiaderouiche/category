317362b7ec8396feca5dabde5e49086aa1dac0f9
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-13 16:29:45 +0100

    +work to fix bug

--- pkgoptions/options.mk
+++ pkgoptions/options.mk
@@ -1,6 +1,9 @@
 # $NetBSD$
 # XXX
 # XXX This file was created automatically using pkgoptions-1.0.
+# XXX After this fil]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\# $NetBSD$
+# XXX
+# XXX This file was created automatically using pkgoptions-1.0.
 # XXX After this file has been verified as correct, the comment lines
 # XXX beginning with "XXX" should be removed.  Please do not commit
 # XXX unverified options.mk files.
@@ -8,3 +11,7 @@
 # XXX
 
 BUILDLINK_PKGSRCDIR.pkgoptions?=	../../category/pkgoptions
+.endif	# PKGOPTIONS_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-pkgoptions
+]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\[H[2J[3J]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\[H[2J[3J]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\[H[2J[3J]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\[H[2J[3J]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\[H[2J[3J]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\]0;jihbed@Hattin:/usr/pkgsrc/category/pkgoptions\]7;file://Hattin/usr/pkgsrc/category/pkgoptions\
