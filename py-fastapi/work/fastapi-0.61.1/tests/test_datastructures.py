593ac7ab084e824fa3c254e585ee9b77d7552834
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-14 11:06:55 +0100

    +prepare future pkg

--- py-fastapi/work/fastapi-0.61.1/tests/test_datastructures.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from fastapi import UploadFile
-
-
-def test_upload_file_invalid():
-    with pytest.raises(ValueError):
-        UploadFile.validate("not a Starlette UploadFile")
--- py-fastapi/work/fastapi-0.61.1/tests/utils.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-import pytest
-
-skip_py36 = pytest.mark.skipif(sys.version_info < (3, 7), reason="skip python3.6")
