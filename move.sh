f5b9d00b66328eb470cd3d1ccaf271c477283ba3
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-21 10:30:02 +0100

    +set folder

--- move.sh
+++ move.sh
@@ -4,5 +4,5 @@
 cd /usr/pkgsrc/wip
 for pdir in py-gerrychain py-graphene-sqlalchemy py-graphene py-grip py-path-and-address py-postgresfixture py-selenium-base py-wasserplan py-zfec py-teamcity py-jose
 do
-    mv -v $pdir /usr/pkgsrc/catgeory/DONE
+    mv -v $pdir /usr/pkgsrc/category/DONE
 done
