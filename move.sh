4337dfc497b8d525fbcc53b3fb615f0dcca3948e
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-21 11:52:27 +0100

    +set Dependency

--- move.sh
+++ move.sh
@@ -2,7 +2,9 @@
 #
 
 cd /usr/pkgsrc/wip
-for pdir in py-gerrychain py-graphene-sqlalchemy py-graphene py-grip py-path-and-address py-postgresfixture py-selenium-base py-wasserplan py-zfec py-teamcity py-jose
+for pdir in py-gerrychain py-graphene-sqlalchemy py-graphene 
+py-grip py-path-and-address py-postgresfixture py-selenium-base py-wasserplan 
+py-zfec py-teamcity py-jose
 do
     mv -v $pdir /usr/pkgsrc/category/DONE
 done
