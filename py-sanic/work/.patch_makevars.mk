6bd5bc0e9c7367c6e6566efbc817d6e94b6d990e
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-22 15:16:48 +0100

    +set Folder

---
+++ py-sanic/work/.patch_makevars.mk
@@ -0,0 +1,42 @@
+.if !defined(_MAKEVARS_MK)
+_MAKEVARS_MK=	defined
+
+BUILDLINK_PREFIX.dl=	/usr
+BUILDLINK_PREFIX.gettext=	/usr
+BUILDLINK_PREFIX.iconv=	/usr
+BUILDLINK_PREFIX.pthread=	/usr
+BUILDLINK_PREFIX.python37=	/usr/pkg
+BUILTIN_LIB_FOUND.c_r=	no
+BUILTIN_LIB_FOUND.dl=	yes
+BUILTIN_LIB_FOUND.iconv=	no
+BUILTIN_LIB_FOUND.intl=	no
+BUILTIN_LIB_FOUND.pthread=	yes
+BUILTIN_LIB_FOUND.rt=	yes
+H_CITRUS_ICONV=	__nonexistent__
+H_DL=	/usr/include/dlfcn.h
+H_GENTOO_GETTEXT=	__nonexistent__
+H_GETTEXT=	/usr/include/libintl.h
+H_GLIBC_ICONV=	/usr/include/iconv.h
+H_ICONV=	__nonexistent__
+H_NGETTEXT_GETTEXT=	/usr/include/libintl.h
+H_OPNSVR5_GETTEXT=	__nonexistent__
+H_PTHREAD=	/usr/include/pthread.h
+IS_BUILTIN.dl=	yes
+IS_BUILTIN.gettext=	yes
+IS_BUILTIN.iconv=	no
+IS_BUILTIN.pthread=	yes
+TOOLS_DEPENDS.ghostscript=	ghostscript>=6.01:../../print/ghostscript
+USE_BUILTIN.dl=	yes
+USE_BUILTIN.gettext=	yes
+USE_BUILTIN.iconv=	yes
+USE_BUILTIN.pthread=	yes
+_BLNK_DLOPEN_REQUIRE_PTHREADS=	no
+_BLNK_PHYSICAL_PATH.LOCALBASE=	/usr/pkg
+_BLNK_PHYSICAL_PATH.WRKDIR=	/usr/pkgsrc/category/py-sanic/work
+_BLNK_PKG_DBDIR.python37=	/usr/pkg/pkgdb/python37-3.7.9nb1
+_IGNORE_INFO_PATH=	
+_MANCOMPRESSED=	no
+_MANZ=	no
+_USE_TOOLS=	[ awk basename cat chgrp chmod chown cmp cp curl cut date diff digest dirname echo egrep env expr false fgrep find grep gzcat head hostname id install ln ls mkdir mv printf pwd rm rmdir sed sh sort tail tar test touch tr true uname wc xargs
+
+.endif # _MAKEVARS_MK
