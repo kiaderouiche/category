9f45ea4afe4da0780ffedb4c3041bdfe73d23162
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-27 22:54:23 +0100

    +clean folder

--- py-mpi4y-fft/work/mpi4py-fft-2.0.3/mpi4py_fft/fftw/__init__.py
+++
@@ -1,3 +0,0 @@
-from .xfftn import *
-from .factory import get_planned_FFT, export_wisdom, import_wisdom, \
-    forget_wisdom, cleanup, set_timelimit, get_fftw_lib
--- py-mpi4y-fft/work/mpi4py-fft-2.0.3/mpi4py_fft/io/__init__.py
+++
@@ -1,4 +0,0 @@
-from .h5py_file import *
-from .nc_file import *
-from .file_base import *
-from .generate_xdmf import generate_xdmf
