095ad7d6fb12cf435f37153c72b9dd331f09ba8f
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-29 15:57:27 +0100

    patch py-bicho: support Python3

--- py-bicho/patches/patch-setup.py
+++ py-bicho/patches/patch-setup.py
@@ -1,21 +1,19 @@
 $NetBSD$
 
---- setup.py.orig	2011-06-28 10:58:38.000000000 +0000
+--- setup.py.orig	2013-05-03 10:33:44.000000000 +0000
 +++ setup.py
-@@ -21,6 +21,9 @@
- #          Francisco Rivas <frivas@libresoft.es>
+@@ -22,6 +22,7 @@
  #          Luis Cañas Díaz <lcanas@libresoft.es>
  #
-+
-+import sys 
-+
  from distutils.core import setup
++import sys
  
  setup(name = "Bicho",
-@@ -30,5 +33,5 @@ setup(name = "Bicho",
-       description = "Analysis tool for Issue/Bug Tracking Systems",
+       version = "0.9",
+@@ -31,5 +32,5 @@ setup(name = "Bicho",
        url = "https://projects.libresoft.es/projects/bicho",      
-       packages = ['Bicho', 'Bicho.backends', 'Bicho.db'],
+       packages = ['Bicho', 'Bicho.backends', 'Bicho.db',
+                   'Bicho.post_processing'],
 -      data_files = [('share/man/man1/',['doc/bicho.1'])],
 -      scripts = ["bin/bicho"])
 +      data_files = [('man/man1/',['doc/bicho.1'])],
