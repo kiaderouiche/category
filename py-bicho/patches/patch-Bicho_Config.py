d3eddddadbca0f2990d6d36815b42cbc7f3de94f
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-10-29 16:26:43 +0100

    py-bicho: PLIST update

--- py-bicho/patches/patch-Bicho_Config.py
+++ py-bicho/patches/patch-Bicho_Config.py
@@ -9,7 +9,7 @@ $NetBSD$
 -from backends import Backend
 -import info
 +from .backends import Backend
-+import .info
++from .info import VERSION, DESCRIPTION
  from optparse import OptionGroup, OptionParser
  import os
  import pprint
@@ -42,3 +42,14 @@ $NetBSD$
              print ("Not an URL: "  + Config.url)
              
                  
+@@ -134,8 +134,8 @@ class Config:
+         """
+         """
+         
+-        parser = OptionParser(usage=usage, description=info.DESCRIPTION,
+-                              version=info.VERSION)
++        parser = OptionParser(usage=usage, description=DESCRIPTION,
++                              version=VERSION)
+ 
+         # General options
+         parser.add_option('-b', '--backend', dest='backend',
