69823e105554a96e32ae3d1c181c2e0c8c7136eb
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2020-11-30 21:50:50 +0100

    ADDED PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, ADDED NEW DEPENDENCE/S, DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- py-django-reactive/work/django-reactive-0.0.8/django_reactive/widget/fields.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.postgres.forms.jsonb import JSONField
-
-from .widgets import ReactJSONSchemaFormWidget
-
-
-class ReactJSONSchemaFormField(JSONField):
-    widget = ReactJSONSchemaFormWidget
