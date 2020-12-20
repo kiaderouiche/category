08915bdd90c8a888a1dc5ce934bfc8d2f44c7642
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2020-12-20 22:36:40 +0100

    ADDED PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG, UPDATE PKGS,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- py-flatbuffers/patches/patch-setup.py
+++ py-flatbuffers/patches/patch-setup.py
@@ -2,15 +2,16 @@ $NetBSD$
 
 --- setup.py.orig	2019-05-31 18:29:30.000000000 +0000
 +++ setup.py
-@@ -43,7 +43,6 @@ def version():
+@@ -43,7 +43,7 @@ def version():
  
  setup(
      name='flatbuffers',
 -    version=version(),
++    version= 1.12,
      license='Apache 2.0',
      author='FlatBuffers Contributors',
      author_email='me@rwinslow.com',
-@@ -68,4 +67,4 @@ setup(
+@@ -68,4 +68,4 @@ setup(
          'Documentation': 'https://google.github.io/flatbuffers/',
          'Source': 'https://github.com/google/flatbuffers',
      },
