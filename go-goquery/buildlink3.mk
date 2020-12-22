9dee548a294067a5c0ef58190bf14f384c64d562
K.I.A Derouiche <kamel.derouiche@gmail.com>
2020-12-22 23:46:44 +0100

    Update buildlink3.mk

--- go-goquery/buildlink3.mk
+++ go-goquery/buildlink3.mk
@@ -1,14 +1,4 @@
 # $NetBSD$
-# XXX
-# XXX This file was created automatically using createbuildlink-3.18.
-# XXX After this file has been verified as correct, the comment lines
-# XXX beginning with "XXX" should be removed.  Please do not commit
-# XXX unverified buildlink3.mk files.
-# XXX
-# XXX Packages that only install static libraries or headers should
-# XXX include the following line:
-# XXX
-# XXX   BUILDLINK_DEPMETHOD.go-goquery?=        build
 
 BUILDLINK_TREE+=        go-goquery
 
@@ -19,7 +9,7 @@ BUILDLINK_CONTENTS_FILTER.go-goquery=   ${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-goquery?=                build
 
 BUILDLINK_API_DEPENDS.go-goquery+=      go-goquery>=1.6.0
-BUILDLINK_PKGSRCDIR.go-goquery?=        ../../category/go-goquery
+BUILDLINK_PKGSRCDIR.go-goquery?=        ../../wip/go-goquery
 .endif  # GO_GOQUERY_BUILDLINK3_MK
 
 BUILDLINK_TREE+=        -go-goquery
