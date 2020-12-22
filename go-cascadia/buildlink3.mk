a173958bf421e90a119f2a66cfcd40d5d0cfe5e5
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2020-12-23 00:20:45 +0100

    ADDED PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG, UPDATE PKGS,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- go-cascadia/buildlink3.mk
+++ go-cascadia/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	go-cascadia
 GO_CASCADIA_BUILDLINK3_MK:=
 
 BUILDLINK_CONTENTS_FILTER.go-cascadia=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-cascadia?=		build
+BUILDLINK_DEPMETHOD.go-cascadia?=	build
 
 BUILDLINK_API_DEPENDS.go-cascadia+=	go-cascadia>=1.0.0
 BUILDLINK_PKGSRCDIR.go-cascadia?=	../../wip/go-cascadia
