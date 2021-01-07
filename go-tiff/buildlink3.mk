440c9c56d7efc7b800779899868e9808c69a9062
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-07 13:48:21 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- go-tiff/buildlink3.mk
+++ go-tiff/buildlink3.mk
@@ -8,8 +8,12 @@ GO_TIFF_BUILDLINK3_MK:=
 BUILDLINK_CONTENTS_FILTER.go-tiff=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-tiff?=		build
 
-BUILDLINK_API_DEPENDS.go-tiff+=	go-tiff>=20190829
+BUILDLINK_API_DEPENDS.go-tiff+=	go-tiff>=20190829nb1
 BUILDLINK_PKGSRCDIR.go-tiff?=	../../wip/go-tiff
+
+
+.include "../../graphics/go-image/buildlink3.mk"
+.include "../../wip/go-lzw/buildlink3.mk"
 .endif	# GO_TIFF_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-tiff
