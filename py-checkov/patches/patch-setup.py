678f79a1a051030bba359a7b76b6b414565a6a5c
K.I.A.Derouiche <kamelderouiche@yahoo.com>
2021-01-09 00:12:51 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

---
+++ py-checkov/patches/patch-setup.py
@@ -0,0 +1,21 @@
+$NetBSD$
+
+--- setup.py.orig	2021-01-07 22:20:03.000000000 +0000
++++ setup.py
+@@ -1,6 +1,7 @@
+ #!/usr/bin/env python
+ import logging
+ import os
++import sys
+ from importlib import util
+ from os import path
+ 
+@@ -59,7 +60,7 @@ setup(
+     author_email="meet@bridgecrew.io",
+     url="https://github.com/bridgecrewio/checkov",
+     packages=setuptools.find_packages(exclude=["tests*","integration_tests*"]),
+-    scripts=["bin/checkov","bin/checkov.cmd"],
++    scripts=["bin/checkov"+sys.version[0:3]],
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     classifiers=[
