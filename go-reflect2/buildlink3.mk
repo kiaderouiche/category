778f3f6ac0f05c74f9c862eeddd167a2b65c9c19
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-01-20 12:14:54 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- go-reflect2/buildlink3.mk
+++ go-reflect2/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	go-reflect2
 GO_REFLECT2_BUILDLINK3_MK:=
 
 BUILDLINK_CONTENTS_FILTER.go-reflect2=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-reflect2?=		build
+BUILDLINK_DEPMETHOD.go-reflect2?=	build
 
 BUILDLINK_API_DEPENDS.go-reflect2+=	go-reflect2>=1.0.1
 BUILDLINK_PKGSRCDIR.go-reflect2?=	../../wip/go-reflect2
