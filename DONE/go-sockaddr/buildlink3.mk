1cc7cdeaccb602f89b27c83cb74e81cc4e51bca8
K.I.A Derouiche <kamel.derouiche@gmail.com>
2021-01-22 11:29:01 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- DONE/go-sockaddr/buildlink3.mk
+++ DONE/go-sockaddr/buildlink3.mk
@@ -6,11 +6,10 @@ BUILDLINK_TREE+=	go-sockaddr
 GO_SOCKADDR_BUILDLINK3_MK:=
 
 BUILDLINK_CONTENTS_FILTER.go-sockaddr=	${EGREP} gopkg/
-BUILDLINK_DEPMETHOD.go-sockaddr?=		build
+BUILDLINK_DEPMETHOD.go-sockaddr?=	build
 
-BUILDLINK_API_DEPENDS.go-sockaddr+=	go-sockaddr>=1.0.2
-BUILDLINK_PKGSRCDIR.go-sockaddr?=	../../wip/go-sockaddr
-
+BUILDLINK_API_DEPENDS.go-sockaddr+=	go-sockaddr>=1.0.2nb1
+BUILDLINK_PKGSRCDIR.go-sockaddr?=	../../category/go-sockaddr
 
 .include "../../wip/go-errwrap/buildlink3.mk"
 .endif	# GO_SOCKADDR_BUILDLINK3_MK
