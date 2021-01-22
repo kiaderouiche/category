045542675afe53833e99cac36c38f818e2f87eb3
K.I.A Derouiche <kamel.derouiche@gmail.com>
2021-01-22 21:21:48 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- DONE/go-tebata/buildlink3.mk
+++ DONE/go-tebata/buildlink3.mk
@@ -9,7 +9,7 @@ BUILDLINK_CONTENTS_FILTER.go-tebata=	${EGREP} gopkg/
 BUILDLINK_DEPMETHOD.go-tebata?=		build
 
 BUILDLINK_API_DEPENDS.go-tebata+=	go-tebata>=20180602
-BUILDLINK_PKGSRCDIR.go-tebata?=	../../wip/go-tebata
+BUILDLINK_PKGSRCDIR.go-tebata?=		../../wip/go-tebata
 .endif	# GO_TEBATA_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-go-tebata
