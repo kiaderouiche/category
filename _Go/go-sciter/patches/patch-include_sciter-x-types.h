d1d6f6a0b961faddab572b448dbb7fc1edcfba1e
K.I.A Derouiche <kamel.derouiche@gmail.com>
2021-01-22 12:03:43 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

---
+++ _Go/go-sciter/patches/patch-include_sciter-x-types.h
@@ -0,0 +1,15 @@
+$NetBSD$
+
+--- include/sciter-x-types.h.orig	2020-11-29 12:50:56.000000000 +0000
++++ include/sciter-x-types.h
+@@ -75,6 +75,10 @@ enum GFX_LAYER
+   #ifndef LINUX
+   #define LINUX
+   #endif
++#elif defined(__NetBSD__)
++  #ifndef NETBSD
++  #define NETBSD
++  #endif
+ #else
+   #error "This platform is not supported yet"
+ #endif
