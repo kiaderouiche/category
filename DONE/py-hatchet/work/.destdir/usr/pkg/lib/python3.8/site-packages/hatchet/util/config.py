6b3159d4a50c477015329a023ba52a6c1f3be06d
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-02-11 10:50:12 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- DONE/py-hatchet/work/.destdir/usr/pkg/lib/python3.8/site-packages/hatchet/util/config.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2017-2020 Lawrence Livermore National Security, LLC and other
-# Hatchet Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: MIT
-
-dot_keywords = ["graph", "subgraph", "digraph", "node", "edge", "strict"]
