b9ca60bea92c279cf537ad7ddacd37b92afa4510
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-03-02 16:16:59 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- py-manopt/options.mk
+++ py-manopt/options.mk
@@ -1,19 +1,16 @@
-# $NetBSD: options.mk,v 1.6 2013/08/01 18:44:06 riastradh Exp $
 
 PKG_OPTIONS_VAR=	PKG_OPTIONS.pymanopt
-PKG_SUPPORTED_OPTIONS=	py-autograd py-tensorflow py-theano
-PKG_SUGGESTED_OPTIONS=	py-autograd py-theano
+PKG_SUPPORTED_OPTIONS=	py-autograd py-tensorflow py-Theano
+PKG_SUGGESTED_OPTIONS=	py-autograd py-Theano
 
 .include "../../mk/bsd.options.mk"
 
 .if !empty(PKG_OPTIONS:Mpy-autograd)
-.include "../../math/glpk/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-glpk
+DEPENDS+=	${PYPKGPREFIX}-autograd>=1.3:../../math/py-autograd
+CONFIGURE_ARGS+=        --with-py-autograd
 .endif
 
 .if !empty(PKG_OPTIONS:Mpy-theano)
-.include "../../devel/hdf5/buildlink3.mk"
-.else
-CONFIGURE_ARGS+=	--without-hdf5
+DEPENDS+=	${PYPKGPREFIX}-Theano>=1.0.4:../../math/py-Theano
+CONFIGURE_ARGS+=        --with-py-Theano
 .endif
