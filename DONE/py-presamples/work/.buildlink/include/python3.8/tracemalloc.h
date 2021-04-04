81637d93f1eea5efeaccb79a7590c06f2c555407
K.I.A.Derouiche <kamel.derouiche@gmail.com>
2021-04-04 19:00:35 +0100

    ADD/REMOVE/UPDATE PKG/PKGS, SET {LICENSE, CATEGORY, MAINTAINER, HOMEPAGE, COMMENT, MAKEFILE}, RENAME PKGS/PKGS, ADDED NEW DEPENDENCE/S, ADDED PATCHES, REMOVE PATCHES_ORIG,CLEAN WORKDIR,DONE PKG/PKGS, MOVE TO DONE, MOVE TO WIP

--- DONE/py-presamples/work/.buildlink/include/python3.8/tracemalloc.h
+++
@@ -1,1 +0,0 @@
-/usr/pkg/include/python3.8/tracemalloc.h
--- DONE/py-presamples/work/presamples-0.2.7/presamples/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .fixed_sum import FixedSum
-from .kronecker_delta import KroneckerDelta
-
-try:
-    from .parameterized import ParameterizedBrightwayModel
-except ImportError:
-    # Brightway2-data not installed
-    pass
